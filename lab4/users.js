b043510f9ccc7facc27e6c326532e055b84cf860
Abhijeet singh <abhijeetsingh7752@gmail.com>
2026-09-11 15:42:38 +0530

    apicreated

---
+++ lab4/users.js
@@ -0,0 +1,9 @@
+// We use in memory data base
+let users =[
+    {id:1,name:'Abhijeet singh',mob:'8470xxxxxx',email:'exp.abhi@exam.com'},
+    {id:1,name:'Abhishek singh',mob:'8845xxxxxx',email:'panday.abhi@exam.com'},
+]
+
+let nextId=3;
+
+export const getUsers=()=>users;
